650b0d2baaef0b91c856d62ed0bb3b696df58e8f
Andreja Nesic <andreja.nesic@brerasolutions.com>
2021-11-19 16:25:46 +0100

    mala ispravka

--- src/main/java/com/raf/sk/specification/exceptions/IODriverException.java
+++ src/main/java/com/raf/sk/specification/exceptions/IODriverException.java
@@ -4,4 +4,8 @@ package com.raf.sk.specification.exceptions;
  * Za greške prilikom izvršavanja IODriver operacija.
  */
 public class IODriverException extends RuntimeException {
+
+    public IODriverException(String s) {
+        super(s);
+    }
 }
